828f996accd14279b71d5b2097223df41ddc6af1
MacielDev <andreraizdetudo@gmail.com>
2023-02-13 18:22:34 -0300

    rateLimiter is on

--- src/config/cash.ts
+++ src/config/cash.ts
@@ -12,8 +12,8 @@ export default {
   //Objeto de configuracão do Redis
   config: {
     redis: {
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT,
+      host: process.env.REDIS_HOST || undefined,
+      port: process.env.REDIS_PORT || undefined,
       password: process.env.REDIS_PASS || undefined,
     },
   },
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,6 +7,7 @@ import { errors } from 'celebrate';
 //import { pagination } from 'typeorm-pagination';
 import AppError from '@shared/errors/AppError';
 import '@shared/infra/typeorm';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 
 const app = express();
 
@@ -15,6 +16,8 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+app.use(rateLimiter);
 
 app.use('/files', express.static(uploadConfig.directory));
 
